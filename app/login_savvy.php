6c379adb8837b42463f93ba93ca3baac6a9ced85
kibambe-0167 <kibambe.0167@gmail.com>
2021-05-10 04:57:35 +0200

    Savvy login and registrations complete

--- app/login_savvy.php
+++ app/login_savvy.php
@@ -18,8 +18,31 @@
 
       // check for empty field of email.
       if( !empty( $_POST["email"] ) ) {
-        echo "Email field not empty.";
+        // echo "Email field NOT empty.";
+        $email = strtolower( $_POST["email"] ); // make lowercase.
+
+        // make query to check if email is available in the db.
+        $query = "select * from Savvy where email='$email'";
+        $result = mysqli_query( $connObj, $query ); // run the query in the db obj
+
+        if( mysqli_num_rows( $result ) == 1 ) {
+          // print_r( $result );
+          $savvy = mysqli_fetch_assoc( $result );
+          print_r( $savvy );
+
+          // make session variables to send this data to all pages about
+          // the current user.
+          // $_SESSION["savvy_id"] = $savvy["id"];
+          // $_SESSION["savvy_fname"] = $savvy["firstname"];
+          // $_SESSION["savvy_lname"] = $savvy["lastname"];
+          // $_SESSION["savvy_email"] = $savvy["email"];
+        }
+
+        else { // when email not found in db.
+          echo "Email NOT found in db";
+        }
       }
+      
       else { // email field is empty
         echo "Email field is empty";
       }
